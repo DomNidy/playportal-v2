7c16cd69979859f67cceca3770205f9b3e53ef78
Dominic Nidy <dominicnidy3@gmail.com>
2024-05-07 14:27:33 -0400

    Testing out ratelimiting

---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import { NextResponse, type NextRequest } from "next/server";
+// import { updateSession } from "~/utils/supabase/middleware";
+
+const requestCounter: Record<string, number> = {};
+
+// TODO: Figure out if we need the updateSession middleware const req = await updateSession(request)
+export default function middlware(request: NextRequest) {
+  // TODO: Implement rate limiting here
+  const ip = (request.headers.get("x-forwarded-for") ?? "127.0.0.1").split(
+    ",",
+  )[0];
+  const clientIp = { ip }.ip ?? "127.0.0.1";
+
+  requestCounter[clientIp] = (requestCounter[clientIp] ?? 0) + 1;
+  console.log("Request counts", requestCounter);
+
+  const response = NextResponse.next();
+  return response;
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
