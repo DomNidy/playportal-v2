98cc97fce8ad08426c2a92d79ed37fabf5b8451a
Dominic Nidy <dominicnidy3@gmail.com>
2024-06-06 11:27:14 -0400

    Updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { getURL } from "~/utils/utils";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     { url: `${getURL()}`, lastModified: new Date(), priority: 1 },
-    { url: `${getURL()}/downloads`, lastModified: new Date(), priority: 0.2 },
+    { url: `${getURL()}/downloads`, lastModified: new Date(), priority: 0.45 },
     { url: `${getURL()}/support`, lastModified: new Date(), priority: 0.2 },
   ];
 }
