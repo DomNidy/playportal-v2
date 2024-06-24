c2c2941041f6ca721c201658a10f22bd2556406e
Dominic Nidy <dominicnidy3@gmail.com>
2024-06-24 13:36:51 -0400

    Removed p element showing the raw json for text overlay options

--- src/components/ui/CreateVideoForm/steps/CreateVideoFormSubmitStep.tsx
+++ src/components/ui/CreateVideoForm/steps/CreateVideoFormSubmitStep.tsx
@@ -179,8 +179,6 @@ export default function CreateVideoFormSubmitStep() {
         </Button>
       </div>
 
-      <p>{JSON.stringify(textOverlayFormStep)}</p>
-
       {submitError && <p>{submitError}</p>}
 
       {isUploadingFiles && (
